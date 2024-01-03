c8fb2bd319c5195e1b655ed85a1d6e2f6f9f9a55
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2024-01-02 17:35:02 -0800

    commiting complete project, did it in the module accidentally

---
+++ index.js
@@ -0,0 +1,71 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter the project description:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'GPL', 'Apache', 'BSD', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'what is the script to run tests:',
+    default: '`npm run test`',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data);
+}
+
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const markdown = generateMarkdown(answers);
+      writeToFile('README.md', markdown);
+      console.log('README.md successfully generated!');
+    })
+    .catch((error) => console.error(error));
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,69 @@
+function renderLicenseBadge(license) {
+  if (!license || license.toLowerCase() === 'none') {
+    return '';
+  }
+
+  return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+}
+
+function renderLicenseLink(license) {
+  if (!license || license.toLowerCase() === 'none') {
+    return '';
+  }
+
+  return `[License](./LICENSE)`;
+}
+
+function renderLicenseSection(license) {
+  if (!license || license.toLowerCase() === 'none') {
+    return '';
+  }
+
+  return `This project is licensed under the ${license} license.`;
+}
+
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `# ${data.title}
+
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${licenseSection}
+${licenseLink}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+`;
+
+}
+
+module.exports = generateMarkdown;
